363ebc062da52e07b8d9377f9dd7fe3c64c5c0d0
Omar Ahmad <omarahmad4@g.ucla.edu>
2019-11-22 16:18:30 -0800

    fixing tab/space issue

--- Erick/Workspace/timing_client/timing_server.py
+++ Erick/Workspace/timing_client/timing_server.py
@@ -14,57 +14,50 @@ port = 1
 server_sock.bind(("",port))
 server_sock.listen(1)
 
-while True:
-    # Wait for a connection
-    print('waiting for a connection')
-    connection, client_address = server_sock.accept()
-    GPIO.output(18,GPIO.HIGH)
-    try:
-        # Receive the data         
-        data = connection.recv(1024)
-        print(data)
-        current = time.time()
-        #print(current)
-        
-        parsed = data.split(',')
-        diff = float(parsed[1])
-        expected_time = float(parsed[0])
-        #print(diff)
-        latency = current - diff
-        #print(latency)
-        
-        final = time.time() - latency
-                
-        while(final < expected_time):
-            #print(final)
-            #time.sleep(1)
-            final = time.time() - latency
-        print('out')
+while 1:	#Wait for a connection
+	print('waiting for a connection')
+	connection, client_address = server_sock.accept()
+	GPIO.output(18,GPIO.HIGH)
+	try:
+		# Receive the data         
+		data = connection.recv(1024)
+		print(data)
+		current = time.time()
+		#print(current)
+		
+		parsed = data.split(',')
+		diff = float(parsed[1])
+		expected_time = float(parsed[0])
+		#print(diff)
+		latency = current - diff
+		#print(latency)
+		
+		final = time.time() - latency
+		
+		while(final < expected_time):
+			#print(final)
+			#time.sleep(1)
+			final = time.time() - latency
+			print('out')
 
-	while True:
-		print('ON')
-		GPIO.output(18, GPIO.HIGH)
-		time.sleep(SLEEP_TIME)
-		print('OFF')
-		GPIO.output(18, GPIO.LOW)
-		time.sleep(SLEEP_TIME)
-        
-    finally:
-        # Clean up the connection
-#        print("Closing current connection")
-        connection.close()
+		while True:
+			print('ON')
+			GPIO.output(18, GPIO.HIGH)
+			time.sleep(SLEEP_TIME)
+			print('OFF')
+			GPIO.output(18, GPIO.LOW)
+			time.sleep(SLEEP_TIME)
 
-    # instantiate reconnection
-    #server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    
-    #port = 1
-    #server_sock.bind(("",port))
-    #server_sock.listen(1)  
+	finally:
+		# Clean up the connection
+		#        print("Closing current connection")
+		connection.close()
 
-    
+	# instantiate reconnection
+	#server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-
-
-
+	#port = 1
+	#server_sock.bind(("",port))
+	#server_sock.listen(1)  
 
 
