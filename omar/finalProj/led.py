f8a06a7d7d691a6baa828b2a9cbdd008858a6452
Omar Ahmad <omarahmad4@g.ucla.edu>
2020-02-05 13:49:21 -0800

    working led.py - have to call with python3 even if pyton is on python3

--- omar/finalProj/led.py
+++ omar/finalProj/led.py
@@ -1,5 +1,17 @@
 import board
 import neopixel
+import time
 
 pixels = neopixel.NeoPixel(board.D18, 12)
 pixels.fill((255, 255, 255))
+
+try:
+    while True:
+        pixels.fill((255,0,0))
+        time.sleep(0.5)
+        pixels.fill((0,255,0))
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    print("\nCleaning up")
+    pixels.fill((0,0,0))
