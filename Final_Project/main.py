9b6b6a6a95daa1958d89d7874d4ad0949e202a49
Omar Ahmad <omarahmad4@g.ucla.edu>
2020-02-19 20:39:25 -0800

    working voice, background sensor pinging, and question administering(including moving players back to start if they move)

---
+++ Final_Project/main.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+
+
+
+
+
+#main program scratch
+
+
+player1Dist = 0
+player2Dist = 0
+
+RED = 0 #red light output
+GREEN = 0 # green light output
+
+def changeLight(color):
+    #change the light to red or green
+    GPIO
+
+def getDist(player):
+    
+    
+
+changeLight(RED)
+while 1:
+    #change light red to green and back and measure dist of players
+    startPlayer1Dist = getDist(1)
+    startPlayer2Dist = getDist(2)
+    
+
+
