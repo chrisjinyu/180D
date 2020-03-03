023a7010a82b4b96873587a9f676f0bd2976d184
Yibin-Gu <55898791+Yibin-Gu@users.noreply.github.com>
2020-03-03 13:08:29 -0800

    Update Listen.py

--- Final_Project/Listen.py
+++ Final_Project/Listen.py
@@ -1,7 +1,7 @@
 import  speech_recognition  as  sr
 
-P1_INDEX = 1
-P2_INDEX = 5
+P1_INDEX = 3
+P2_INDEX = 3
 
 
 
@@ -10,23 +10,27 @@ r1 = sr.Recognizer()
 def listen(player):
     try:
         if (player == 1):
-            #print('bose')
+            print('connecting to p1 mic')
             r1 = sr.Recognizer()
+            print("a")
             with  sr.Microphone(device_index=P1_INDEX)  as source:
+                print("b")
                 r1.adjust_for_ambient_noise(source)
                 print('Player one speak')
-                audio = r1.listen(source)
+                audio = r1.listen(source, phrase_time_limit = 2, timeout = 2)
                 print('processing...')
                 get = r1.recognize_google(audio)
                 print(get)
          
         elif (player == 2):
-            #print('v8')
+            print('connecting to p2 mic')
             r1 = sr.Recognizer()
+            print("b")
             with  sr.Microphone(device_index=P2_INDEX) as source:
+                print("c")
                 r1.adjust_for_ambient_noise(source)
                 print('Player two speak')
-                audio = r1.listen(source)
+                audio = r1.listen(source, phrase_time_limit = 2, timeout = 2)
                 print('processing...') 
                 get = r1.recognize_google(audio)
                 print(get)
