a5c61e2b9953de5b75fa8af054d4ca5919e66527
omarahmad4 <48375656+omarahmad4@users.noreply.github.com>
2020-03-16 11:47:52 -0700

    Add files via upload

--- Final_Project/working/Listen.py
+++ Final_Project/working/Listen.py
@@ -1,49 +1,42 @@
 import  speech_recognition  as  sr
 
-P1_INDEX = 1
+P1_INDEX = 3
 P2_INDEX = 3
 
-PHRASE_TIME_LIMIT = 4
-TIMEOUT = 4
+
 
 r1 = sr.Recognizer()
 
 def listen(player):
-	try:
-		if (player == 1):
-			#print('bose')
-			r1 = sr.Recognizer()
-			with  sr.Microphone(device_index=P1_INDEX)  as source:
-				r1.adjust_for_ambient_noise(source)
-				print('Player one speak')
-				audio = r1.listen(source, phrase_time_limit = PHRASE_TIME_LIMIT, timeout = TIMEOUT)
-				print('processing...')
-				get = r1.recognize_google(audio)
-				return get
-				#print(get)
-		 
-		elif (player == 2):
-			#print('v8')
-			r1 = sr.Recognizer()
-			with  sr.Microphone(device_index=P2_INDEX) as source:
-				r1.adjust_for_ambient_noise(source)
-				print('Player two speak')
-				audio = r1.listen(source, phrase_time_limit=PHRASE_TIME_LIMIT, timeout = TIMEOUT)
-				print('processing...') 
-				get = r1.recognize_google(audio)
-				return get
-				#print(get)
-				
-	except  sr.UnknownValueError:
-		print('failed to understand')
-		return "null"
-		#return "null"
-	except  sr.RequestError  as e:
-		return "null"
-		print('failed'.format(e))
-		#return "null"
-	except sr.WaitTimeoutError:
-		return "null"
-		print ("never run")
-	#finally:
-		#return "null"
+    try:
+        if (player == 1):
+            print('connecting to p1 mic')
+            r1 = sr.Recognizer()
+            print("a")
+            with  sr.Microphone(device_index=P1_INDEX)  as source:
+                print("b")
+                r1.adjust_for_ambient_noise(source)
+                print('Player one speak')
+                audio = r1.listen(source, phrase_time_limit = 2, timeout = 2)
+                print('processing...')
+                get = r1.recognize_google(audio)
+                print(get)
+         
+        elif (player == 2):
+            print('connecting to p2 mic')
+            r1 = sr.Recognizer()
+            print("b")
+            with  sr.Microphone(device_index=P2_INDEX) as source:
+                print("c")
+                r1.adjust_for_ambient_noise(source)
+                print('Player two speak')
+                audio = r1.listen(source, phrase_time_limit = 2, timeout = 2)
+                print('processing...') 
+                get = r1.recognize_google(audio)
+                print(get)
+                
+    except  sr.UnknownValueError:
+        print('failed to understand')
+    except  sr.RequestError  as e:
+        print('failed'.format(e))
+ 
